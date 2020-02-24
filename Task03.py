3c792364c7a17657e0024fb7574afc3767e3cbe2
Yulia Kuznetsova <dary85@mail.ru>
2020-02-24 19:42:32 +0300

    Удалила лишние файлы

--- Task03.py
+++
@@ -1,7 +0,0 @@
-n = input("Enter integer value: ")
-
-value_to_sum1 = int(n + n)
-value_to_sum2 = int(n + n + n)
-value_to_sum3 = int(n)
-
-print("Result: %d" % (value_to_sum3 + value_to_sum1 + value_to_sum2))
--- Task04.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Input value: "))
-a = 0
-while n > 0:
-    i = n % 10
-    n = n // 10
-    if a < i:
-        a = i
-print("a %d" % a)
